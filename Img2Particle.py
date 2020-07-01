8535e992f0fc4d64c8439fa67492d40732b04943
Yuan Chiang <qaqowotut@gmail.com>
2020-07-02 00:15:33 +0800

    Create project

---
+++ Img2Particle.py
@@ -0,0 +1,17 @@
+import argparse
+
+version = 1
+state = 0
+
+parser = argparse.ArgumentParser(prog='Img2Particle',
+                                 description='Convert images into particle models.')
+
+parser.add_argument('infile', metavar='images', type=str,
+                    help='absolute or relative address of image files')
+parser.add_argument('-o', dest='outfile', metavar='data file', type=argparse.FileType('w'))
+
+parser.add_argument('--version', action='version', version='%(prog)s {:d}.{:d}'.format(version,state))
+
+
+args = parser.parse_args()
+print(args)
