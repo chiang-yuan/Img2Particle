674472653aae2d9229af0cf9c32407624bec6a69
Yuan Chiang <qaqowotut@gmail.com>
2020-07-02 00:21:11 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 
-version = 1
-state = 0
+version = '1.0'
 
 parser = argparse.ArgumentParser(prog='Img2Particle',
                                  description='Convert images into particle models.')
@@ -10,7 +9,9 @@ parser.add_argument('infile', metavar='images', type=str,
                     help='absolute or relative address of image files')
 parser.add_argument('-o', dest='outfile', metavar='data file', type=argparse.FileType('w'))
 
-parser.add_argument('--version', action='version', version='%(prog)s {:d}.{:d}'.format(version,state))
+parser.add_argument('--version', action='version', version='%(prog)s {:s}'.format(version))
+
+
 
 
 args = parser.parse_args()
